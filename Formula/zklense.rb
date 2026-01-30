01c6c5b7cd3abf9e3f6e15404e857f78a95d1c88
gihanrcg <gihanrcg1997@gmail.com>
2026-01-30 15:41:30 +0530

    Update zklense to v0.1.3

--- Formula/zklense.rb
+++ Formula/zklense.rb
@@ -1,18 +1,18 @@
 class Zklense < Formula
   desc "A command-line tool for profiling, building, and deploying zero-knowledge proofs built with Noir for Solana Blockchain"
   homepage "https://github.com/jinali98/zk-lense"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT OR Apache-2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/jinali98/zk-lense/releases/download/v0.1.2/zklense-x86_64-apple-darwin.tar.gz"
-      sha256 "sha256:61559ec910e6b4bb3f52b52e7ecaed6d3134c7fca7a18f96113cb21df770d08c"
+      url "https://github.com/jinali98/zk-lense/releases/download/v0.1.3/zklense-x86_64-apple-darwin.tar.gz"
+      sha256 "552a743546346e28cf66332cdf131afe2982bb60ec1dc1296f52d4463397b234"
     end
 
     on_arm do
-      url "https://github.com/jinali98/zk-lense/releases/download/v0.1.2/zklense-aarch64-apple-darwin.tar.gz"
-      sha256 "sha256:258e8eb7eaf788307010172c7c0c9a3d5f995f2336e8611af162fcbe13114438"
+      url "https://github.com/jinali98/zk-lense/releases/download/v0.1.3/zklense-aarch64-apple-darwin.tar.gz"
+      sha256 "4da54dcb8e2cbb283968fa79c333000eb5b3b862f0acbabbcfdde4a4633b53ba"
     end
   end
 
